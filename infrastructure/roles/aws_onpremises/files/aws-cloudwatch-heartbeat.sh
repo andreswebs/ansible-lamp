d67f29aa0581841931c09ca7cdab53f50d91ae09
andreswebs <andre@particle41.com>
2019-09-11 15:50:38 -0300

    feat: add custom heartbeat metric

---
+++ infrastructure/roles/aws_onpremises/files/aws-cloudwatch-heartbeat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NAMESPACE="OnPremises/heartbeat"
+
+METRIC_DATA="MetricName=heartbeat,Dimensions=[{Name=Hosts,Value=${HOSTNAME}}],Value=1"
+
+aws cloudwatch put-metric-data --namespace "${NAMESPACE}" --metric-data "${METRIC_DATA}"
+
+echo "${METRIC_DATA}" > /root/heartbeat

3d15409e1eacfc08222085a6e01cc6d2acc852e8
andreswebs <andre@particle41.com>
2019-09-11 16:01:51 -0300

    update: remove echo from heartbeat script

--- infrastructure/roles/aws_onpremises/files/aws-cloudwatch-heartbeat.sh
+++ infrastructure/roles/aws_onpremises/files/aws-cloudwatch-heartbeat.sh
@@ -5,5 +5,3 @@ NAMESPACE="OnPremises/heartbeat"
 METRIC_DATA="MetricName=heartbeat,Dimensions=[{Name=Hosts,Value=${HOSTNAME}}],Value=1"
 
 aws cloudwatch put-metric-data --namespace "${NAMESPACE}" --metric-data "${METRIC_DATA}"
-
-echo "${METRIC_DATA}" > /root/heartbeat
